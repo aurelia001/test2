5cb4c923b745320c1418a7cc0ea4107a38d34d92
AureChan <77936166+aurelia001@users.noreply.github.com>
2021-08-20 13:36:04 +0200

    github link

---
+++ src/Weather.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <body>
+      <div className="container">
+        <div className="wrapper">
+          <form id="search-form">
+            <input
+              type="text"
+              autocomplete="off"
+              placeholder="Enter your city..."
+              id="search-text-input"
+            />
+            <input type="submit" value="Submit" />
+            <input type="submit" value="Current" id="current-button" />
+          </form>
+
+          <h1>Paris</h1>
+          <div className="row">
+            <div className="col">
+              <span className="time" id="date">
+                {" "}
+                Sunday 16:00{" "}
+              </span>
+              <br />
+              <span className="description" id="weather-description">
+                {" "}
+                Sunny{" "}
+              </span>
+            </div>
+            <div className="col weather-today">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+                alt="Sunny"
+                id="current-icon"
+              />
+              <span className="temperture-today" id="current-temperture"></span>
+              <span className="celsius">C°</span>
+            </div>
+            <div className="col">
+              <div className="humidity">
+                Humidity: <span id="humidity"></span>
+              </div>
+              <div className="wind">
+                Wind: <span id="windspeed"></span>
+              </div>
+            </div>
+            <div id="forecast"></div>
+          </div>
+        </div>
+      </div>
+      <p className="refrence">
+        <a
+          href="https://github.com/aurelia001/weather-app"
+          target="_blank "
+          className="refrence-link"
+        >
+          Open-sourced code
+        </a>
+        , by Aurelia
+      </p>
+    </body>
+  );
+}
